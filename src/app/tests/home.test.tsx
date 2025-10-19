eda610dc64819d471d2323704f14217e0c66e141
rithikkulkarni <rithik.r.kulkarni@gmail.com>
2025-10-19 11:52:38 -0400

    Fixed vitest test imports in trivial test file

--- src/app/tests/home.test.tsx
+++ src/app/tests/home.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import Home from '../page'
+import { expect, test } from 'vitest'
 
 test('renders Host and Join sections', () => {
   render(<Home />)
